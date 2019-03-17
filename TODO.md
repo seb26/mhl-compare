ecad2cd4f63801c187289ffbb8deb0f2be571ca5
Sebastian Reategui <931319+seb26@users.noreply.github.com>
2019-03-17 12:07:23 -0500

    progress

--- TODO.md
+++ TODO.md
@@ -1,4 +1,11 @@
-# Plan
+# Project goal
 
 * To compare one MHL file's hashes, with another MHL file's hashes.
 * This is different to comparing MHL hashes to the physical files.
+
+
+# Todo
+* Work out what should the order be:
+  * Compare filepath, filename, then hash
+  * Compare filename, filepath, then hash
+  * Compare hash, then filepath, then filename
--- test.py
+++ test.py
@@ -1,25 +1,56 @@
+import os
+import pprint
 import xmltodict
 
-f1 = open('example1.mhl', 'r')
-f2 = open('example2.mhl', 'r')
-f3 = open('example3.mhl', 'r')
+pp = pprint.PrettyPrinter(indent=1)
 
-example1 = xmltodict.parse( f1.read(), dict_constructor=dict )
-example2 = xmltodict.parse( f2.read(), dict_constructor=dict )
-example3 = xmltodict.parse( f3.read(), dict_constructor=dict )
+example1 = open('example1.mhl', 'r')
+example2 = open('example2.mhl', 'r')
+example3 = open('example3.mhl', 'r')
 
-db_a = { f['file']: f for f in example1['hashlist']['hash'] }
-db_b = { f['file']: f for f in example3['hashlist']['hash'] }
+files = {
+    'a': xmltodict.parse( example1.read(), dict_constructor=dict ),
+    'b': xmltodict.parse( example3.read(), dict_constructor=dict )
+}
+
+db = {
+    'a': {},
+    'b': {}
+}
+
+# Set up a dictionary entry for each hash
+
+# For every a, b, c entry listed as a parsed dict
+for letter, parsed in files.items():
+
+    # Find each object in that parsed dict
+    for object in parsed['hashlist']['hash']:
+        # Designate its filename and filepath
+        filepath = object['file']
+        filename = os.path.split( filepath )[1]
+
+        # Then add it to the database
+        # { 'Z.jpg': { 'filepath': 'Path/Z.jpg', 'object': <hash> in Dict format }
+        # According to an identifier. Today, I've selected filepath.
+        identifier = filepath
+        db[letter][identifier] = {
+            'filepath': filepath,
+            'filename': filename,
+            'object': object
+            }
 
 MATCH_COUNT = 0
 
-for bName, bObject in db_b.items():
-    if bName in db_a.keys():
+for bName, bItem in db['b'].items():
+    if bName in db['a'].keys():
         # Found the file successfully
-        bHash = bObject['xxhash64be']
-        aMatchingObject = db_a[bName]
+        aMatching = db['a'][bName]
+        aMatchingObject = aMatching['object']
+        aName = aMatching['filename']
         aHash = aMatchingObject['xxhash64be']
-        aName = aMatchingObject['file']
+
+        bObject = bItem['object']
+        bHash = bObject['xxhash64be']
 
         # Directly compare the hashes
         HASHES_MATCH = True if aHash == bHash else False
@@ -31,3 +62,7 @@ for bName, bObject in db_b.items():
     else:
         # Couldn't find it at all
         print('FAIL: Not present:', bName)
+
+
+print('# END -----------')
+print('Total positive matches:', MATCH_COUNT)
