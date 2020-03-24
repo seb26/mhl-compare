e83699951c63e077fda0fc7e7ba1ac395dee951b
Sebastian Reategui <seb.reategui@gmail.com>
2020-03-25 10:27:52 +1100

    overhaul installation - no more install.commands, which introduced silly security issues on macOS and are not very user friendly either

--- build-zip.sh
+++ build-zip.sh
@@ -1,8 +1,12 @@
-rm -r .tmp
-mkdir .tmp
-mkdir .tmp/bin
-cp dist/mhl-compare ./.tmp/bin/mhl-compare
-cp dist-include/* ./.tmp/
-cp README.md ./.tmp/
-cd .tmp
-zip -9r - * > ../downloads/mhl-compare-$1.zip
+if [[ $1 ]]; then
+  rm -rf .tmp/ || true
+  mkdir .tmp/
+  mkdir .tmp/bin/
+  cp dist/mhl-compare .tmp/mhl-compare
+  cp README.md .tmp/
+  cd .tmp
+  zip -9r -FS - * > ../downloads/mhl-compare-$1.zip
+else
+  echo Please run again and specify the version number as first parameter.
+  exit 1
+fi
--- dist-include/install.command
+++
@@ -1,9 +0,0 @@
-echo "--------------------------------"
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "${DIR}"
-echo "Installing mhl-compare to /usr/local/bin..."
-cp bin/mhl-compare /usr/local/bin
-echo "Done."
-echo "Now you can run it anywhere in a Terminal by typing: mhl-compare"
-echo ""
-echo -e "\x1B[36mYou can now close this window.\x1B[0m"
